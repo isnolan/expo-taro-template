6c8ce02d9ef4d01331656350bc67ec072f82c2d9
yhostc <yhostc@gmail.com>
2023-09-15 20:43:41 +0800

    chore: add new taro

---
+++ taro-new/src/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  pages: ['pages/chat/index', 'pages/setting/index', 'pages/history/index'],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black',
+  },
+});
