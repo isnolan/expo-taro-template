80fa335640978c4573664bf32fa5ca7c14fd3b7e
yhostc <yhostc@gmail.com>
2023-09-17 22:58:33 +0800

    chore: remove taro-new

--- taro-new/src/app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  pages: ['pages/chat/index', 'pages/setting/index', 'pages/history/index'],
-  window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black',
-  },
-});
